b8ba1e2275d4c51b74b8e1497ba25c34697a8314
A-GustavoStori <aldrinstori@gmail.com>
2024-09-14 13:38:01 -0300

    * Removing unused files

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ubuntu:latest
-LABEL authors="dev"
-
-ENTRYPOINT ["top", "-b"]
--- start_streamlit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /media/dev/backup/knowledge
-source venv_streamlit/bin/activate
-streamlit run youtube_transcript_downloader.py
